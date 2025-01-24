1a1ee600b4498e106f9e05427c189c69813d696c
Lprotoo <129598474+Lprotoo@users.noreply.github.com>
2025-01-24 12:10:27 +0100

    prout

--- Personnages/Personnages.py
+++
@@ -1,9 +0,0 @@
-class Personnage:
-    def __init__(self, nom, pv):
-        self.nom = nom
-        self.pv = pv
-
-    def recevoir_degats(self, dgt):
-        self.pv = self.pv - dgt
-
-    
--- main.py
+++
@@ -1,6 +0,0 @@
-import random
-from slprint.slowprint import slowprint
-slowprint ("Bienvenue, veillez choisir un personnage", delay = 0.03, yoyo = False, vertical = False)
-# Mettre l'imput de personnage
-slowprint ("Bienvenue, {nom} vous allez participez à la guerre du saint graal pour se faire, un servant va être invoqué pour se battre à vos cotés", delay = 0.03, yoyo = False, vertical = False)
-# random servant
